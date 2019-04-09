30a8f6ff9d160eccdeaaef2074f21ea4167546a3
PRASAD PONNUSAMY <prasadkovai@gmail.com>
2019-04-09 18:49:52 +0530

    select options

--- src/App.js
+++ src/App.js
@@ -1,51 +1,44 @@
 import React, { Component } from 'react';
-import Dropdown from 'react-bootstrap/Dropdown'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <table>
-        <tr>
-          <td> 
-          <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
-  </Dropdown.Toggle>
-
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
-          </td>
-          <td> <div>This is test app2</div></td>
-          <td> <div>This is test app3</div></td>
-          <td> <div>This is test app4</div></td>
-        </tr>
-        <tr>
-        <td> <div>This is test app11</div></td>
-          <td> <div>This is test app12</div></td>
-          <td> <div>This is test app13</div></td>
-          <td> <div>This is test app14</div></td>
-        </tr>
-        <tr>
-        <td> <div>This is test app21</div></td>
-          <td> <div>This is test app22</div></td>
-          <td> <div>This is test app23</div></td>
-          <td> <div>This is test app24</div></td>
-        </tr>
-        <tr>
-        <td> <div>This is test app31</div></td>
-          <td> <div>This is test app32</div></td>
-          <td> <div>This is test app33</div></td>
-          <td> <div>This is test app34</div></td>
-        </tr>
-      </table>
-      
-      
+        <table>
+          <tr>
+            <td> 
+              <div>
+              <select id="1">
+                <option value="0">APP</option>
+                <option value="1">Mobile</option>
+                <option value="2">Pc</option>
+              </select>
+            </div>
+            </td>
+            <td> <div>This is test app2</div></td>
+            <td> <div>This is test app3</div></td>
+            <td> <div>This is test app4</div></td>
+          </tr>
+          <tr>
+            <td> <div>This is test app11</div></td>
+            <td> <div>This is test app12</div></td>
+            <td> <div>This is test app13</div></td>
+            <td> <div>This is test app14</div></td>
+          </tr>
+          <tr>
+            <td> <div>This is test app21</div></td>
+            <td> <div>This is test app22</div></td>
+            <td> <div>This is test app23</div></td>
+            <td> <div>This is test app24</div></td>
+          </tr>
+          <tr>
+            <td> <div>This is test app31</div></td>
+            <td> <div>This is test app32</div></td>
+            <td> <div>This is test app33</div></td>
+            <td> <div>This is test app34</div></td>
+          </tr>
+        </table>
       </div>
     );
   }
